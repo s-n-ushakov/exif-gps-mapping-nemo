adfdec5b47ceff089ad9e1a8cc4bc7d07054fb80
Sergey Ushakov <s-n-ushakov@yandex.ru>
2023-09-02 03:22:24 +0300

    improve wording in istallation script

--- install-for-current-user.bash
+++ install-for-current-user.bash
@@ -4,5 +4,5 @@
 #=======================================================================================================================
 
 cp --archive --recursive --verbose ./nemo/actions/* "${HOME}/.local/share/nemo/actions" \
-  && echo "$0 : INFO : Deployed to current user profile on local machine successfully."
+  && echo "$0 : INFO : Copied/installed to current user profile on local machine successfully."
 
